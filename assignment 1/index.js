c1538532103a2d3e0995dce28cd0e9510dc6701c
tayyebayasmin <71877340+tayyebayasmin@users.noreply.github.com>
2021-04-15 00:19:53 +0500

    Updated

---
+++ assignment 1/index.js
@@ -0,0 +1,86 @@
+//for-loop
+// var i=0
+// for(var i=0; i<3; i++){
+// console.log('tayyeba')
+// }
+//alert("hello tayyeba");
+//var a = 99 ;
+//document.write(a);
+
+//task 1:
+// var firstname='tayyeba '
+// var lastname='yasmin '
+// var age=20
+// alert("hello "+firstname+lastname+", you are "+age+" year old")
+
+//task 2:
+// var a=2
+// var b=a++ + ++a - --a + a--
+// alert("a= "+a)
+// alert("b= "+b)
+
+
+//task 3:
+// var firstName = prompt("FirstName is :")
+// var lastName = prompt("LastName is :")
+// var age = prompt("Age is:")
+// alert("Hello "+firstName+" "+lastName+","+"you are "+age+" years old")
+
+
+//task 4:
+// var a = parseInt(prompt("enter first number :"))
+// var b = parseInt(prompt("enter second number :"))
+// var operater=prompt("enter op")
+// var c
+// switch(operater){
+//     case '+':
+//     c=a+b
+//     alert(c)
+//     break
+//     var c=a+b;
+//     alert(a + operater + b + "=" + c )
+//     break
+//     case '-':
+//     c=a-b
+//     alert(c)
+//     break
+//     case '*':
+//     c=a*b
+//     alert(c)
+//     break
+//     case '/':
+//     c=a/b
+//     alert(c)
+//     break
+//     default:
+//     alert("invalid input");
+
+//  }
+
+//task 5:
+// var courses = []
+// for(var i=1; i<6; i++)
+// {
+ 
+//  var course = prompt("Enter " + i++ +" course name")
+//  courses.push(course)
+//  }
+//  alert(courses)
+//  for(var j=0;j<5;j++)
+//  {
+//      prompt("Course" + j++, courses[j] )
+//  }
+
+//task 6:
+// var arr=[]
+// var ob=0
+// for(var i=1;i<6;i++)
+// {
+// arr[i]= parseInt(prompt("enter cource"+i+" marks"))
+// ob=arr[i]+ob
+// }
+// var total = parseInt(prompt("enter total marks"))
+// var marks = (ob *100)/total
+// alert(marks)
+
+
---
+++ assignment 2/index.js
@@ -0,0 +1,28 @@
+
+//task 1
+
+// str= `JavaScript and Java are completely different languages, both in concept and design.JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997. ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+// Begin learning here by typing in your first name surrounded by quotation marks, and ending with a semicolon. For example, you could type the name "Jamie";  and then hit enter`
+//  console.log(str);
+
+
+
+//task 2
+var userName='Tayyeba Yasmin'
+var address='pirmahal'
+var email='tayyebajasmin@gmail.com'
+var date='4/14/2021'
+var coverLetter=`${userName}
+${address}
+s${email} 
+${date}
+Dear Ms. Smith,
+I am interested in the author's assistant position at Acme Company, as advertised on LinkedIn. I am currently employed as legislative director for Assemblywoman Nora Kiel, Chairperson of the NYS Assembly. I believe that the skills and experiences I have gained at this position make me an ideal candidate for the job of author’s assistant.
+As legislative director, I have developed strong writing and editing skills. For example, one of my main duties is to prepare Assemblywoman Nora Kiel’s personal legislation, which deals with issues related to her position as Senior Member of the NYS Assembly Standing Committee.
+This duty requires meticulous writing and editing skills, and an ability to convey complex legal ideas clearly. I have prepared dozens of pieces of legislation and received praise for the clarity of my writing.
+I have also gained extensive experience in legal and policy research – fields that you state the author’s assistant must be familiar with. My experience in the NYS Assembly has afforded me the opportunity to become familiar with the consolidated and unconsolidated laws of the State of New York. In particular, through my work with Assemblywoman Nora Kiel, I have become heavily involved in the current welfare and Medicaid reform movement. I am always eager to learn more about state legislation, reading up on these topics on my own time to become more knowledgeable. I would love to bring this passion for policy and law to your company.
+I am confident that my experience in the Legislature and my research and writing skills qualify me for consideration. If you would like, I can provide you with current samples of my work. I have also enclosed my resume. I look forward to meeting with you and discussing my qualifications in more detail.
+Sincerely,
+ ${userName}`
+ console.log(coverLetter);
+ 
